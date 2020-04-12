4b15beea561fe3082574c24bff593f19ac23b7d7
Angel <aleangrodriguez123@gmail.com>
2020-04-12 18:08:19 -0500

    update blog metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `URTipsDev Blog`,
+    author: `Angel Rodriguez`,
+    description: `URTipsDev blog donde explicamos diferentes tips de desarrollo web y movil`,
+    siteUrl: `https://urtips.dev/`,
     social: {
-      twitter: `kylemathews`,
+      youtube: `urtipsdev`,
+      twitter: `urtipsdev`,
     },
   },
   plugins: [
